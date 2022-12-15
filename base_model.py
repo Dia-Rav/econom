30263f39ed714c5e2cc2e16dc0d9e0612d55c2ab
Диана <ravilova.dm@phystech.edu>
2022-12-15 17:17:42 +0300

    gender*marriage

--- base_model.py
+++ base_model.py
@@ -13,9 +13,18 @@ dataset = pd.read_excel ("base.xlsx")
 # model_est = model.fit()
 # print(model_est.summary())
 
-plt.clf()
-sb.scatterplot(dataset, x = "child", y = "salary")
-plt.show()
+
+#возможная альтернатива - добавитб брак* на пол 
+# оказывается что ввобще брак увеличивает зп на 1.018e+04 
+# на для женщин этот рост меньше 
+
+model = smf.ols("salary ~ gender + week + exp +I(exp**2) + gender + degree + marriage + v_age + gender:child + child + gender:marriage + C(sphere)", data=dataset)
+model_est = model.fit()
+print(model_est.summary())
+
+# plt.clf()
+# sb.scatterplot(dataset, x = "child", y = "salary")
+# plt.show()
 
 
 
