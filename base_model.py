193f33e6987a8182279e5962c61e2bb6224b9364
Диана <ravilova.dm@phystech.edu>
2022-12-17 14:33:42 +0300

    тесты и новые диаграммы

--- base_model.py
+++ base_model.py
@@ -6,16 +6,17 @@ from stargazer.stargazer import Stargazer
 import yatg
 import statsmodels.formula.api as smf
 import statsmodels.stats.api as sms
-
+from statsmodels.stats.outliers_influence import variance_inflation_factor  as vif
+# Проверьте чувствительность, качество подгонки данных моделью
 
 dataset = pd.read_excel ("base.xlsx", engine='openpyxl')
 print(dataset.head())
 
-model = smf.ols("salary ~ gender + exp + I(exp**2) + degree + age +C (sphere)", data=dataset)
+model = smf.ols("np.log(salary) ~ gender + exp + I(exp**2) + degree + age + C (sphere)", data=dataset)
 model_est = model.fit()
 print(model_est.summary())
 
-
+#тест Зарембки
 # model = smf.ols("salary_z ~ gender + exp + I(exp**2) + degree + age +C (sphere) + marriage", data=dataset)
 # model_est = model.fit()
 # print(model_est.ssr)
@@ -26,45 +27,77 @@ print(model_est.summary())
 
 # хи = 18.0251854829263...
 
-# критерий Акаике
-# Таким образом, критерий не только вознаграждает за качество приближения, 
-# но и штрафует за использование излишнего количества параметров модели. 
-# Считается, что наилучшей будет модель с наименьшим значением критерия AIC.
+# тест голдфелда квандта
+print(sms.het_goldfeldquandt(y=model.endog, x=model.exog, idx = 2, split=.3, drop=.4))
+# print(sms.het_goldfeldquandt(y=model.endog, x=model.exog, idx = 5, split=.3, drop=.4))
+# (1.0592754435111993, 0.43999116334161426, 'increasing')
+# (1.210007032674096, 0.30258478786068405, 'increasing')
+# The Null hypothesis is that the variance in the two sub-samples are the same. 
+# The alternative hypothesis, can be increasing, 
+# i.e. the variance in the second sample is larger than in the first, or decreasing or two-sided.
+# нет гетерскедостичности 
 
-# информационный критерий шварца 
-# models with lower BIC are generally preferred
 
-# print(sms.het_goldfeldquandt(y=model.endog, x=model.exog,split=.35, drop=.3))
+# тест глейзера 
+# from scipy.stats import chi2
+# model_aux = smf.ols("abs(model_est.resid) ~ degree", data=dataset)
+# model_aux_est = model_aux.fit()
+# stat_aux = model_aux_est.ess / ((1 - 2 / np.pi) * np.var(model_est.resid))
+# print(f"Stat: {stat_aux:5.4f}, Critical value: {chi2.ppf(0.95, df=model_aux.df_model):5.4f}, \
+# p-value: {1 - chi2.cdf(stat_aux, df=model_aux.df_model):5.4f}")
 
-# Тест Бройша-Пагана
+# Stat: 0.1068, Critical value: 3.8415,     p-value: 0.7438 - age
+# Stat: 0.6419, Critical value: 3.8415,     p-value: 0.4230 - gender
+# Stat: 0.9860, Critical value: 3.8415,     p-value: 0.3207 - Exception
+# Stat: 0.6175, Critical value: 3.8415,     p-value: 0.4320 - degree
+# коэффециенты незначимы, гетерскедостичности нет
+
+# plt.clf()
+# sb.scatterplot(dataset, x="salary", y=model_est.resid ** 2)
+# plt.show()
+
+# Тест Бройша-Пагана.
 # print(sms.het_breuschpagan(model_est.resid, model.exog))
-# print(sms.het_breuschpagan(model2_est.resid, model2.exog))
-# print(sms.het_breuschpagan(model3_est.resid, model3.exog))
-# print(sms.het_breuschpagan(model4_est.resid, model4.exog))
-
-# (40.46260269551623, 0.07665277838473605, 1.5106278480053188, 0.0609003187276317)   
-# (42.85443167811152, 0.060349186001638896, 1.5648840811113025, 0.044745809247933456)
-# (37.89817204378537, 0.152381559879282, 1.3307716034567847, 0.13811430776820818)    
-# (41.34440629532407, 0.10142973690863587, 1.432870719521403, 0.08406713932862535)
 # lm: float lagrange multiplier statistic
 # lm_pvalue: float p-value of lagrange multiplier test
 # fvalue: float f-statistic of the hypothesis that the error variance does not depend on x
 # f_pvalue: float p-value for the f-statistic
+# (36.40091234383967, 0.08453871312506729, 1.5008137969287692, 0.07073892887533434)
+# на 10 процентном уровне значимости гетереск нет
 
-# Тест Уайта.
-# print(sms.het_white(model_est.resid, model.exog))
-# print(sms.het_white(model2_est.resid, model2.exog))
-# print(sms.het_white(model3_est.resid, model3.exog))
-# print(sms.het_white(model4_est.resid, model4.exog))
-# (147.5031905491087, 0.2361029144651318, 1.6688611839509147, 0.05149282909355532)
-# (151.6645146225534, 0.3355907412958376, 1.4323123804303264, 0.17024585333421094)
-# (153.77410104351742, 0.3343585088835086, 1.502776041602281, 0.15221652670659727)
-# (158.53002955568007, 0.38455452002003004, 1.4584453862304925, 0.23654095055772012)
-# lm: float The lagrange multiplier statistic. lm_pvalue :float The p-value of lagrange multiplier test.
-# fvalue: float The f-statistic of the hypothesis that the error variance does not depend on x. This is an alternative test variant not the original LM test.
-# f_pvalue: float The p-value for the f-statistic.
+# from chow_test import chow_test
+# print(chow_test(y_series=pd.Series(model.endog),
+# X_series=pd.DataFrame(model.exog), last_index=1499, first_index=1500, significance=0.05))
 
-# plt.clf()
-# sb.scatterplot(dataset, x="v_age", y="salary")
-# sb.lmplot(dataset, x="v_age", y="salary", ci=None)
-# plt.show()
+for i in range(1, model.exog.shape[1]):
+    vif_est = vif(model.exog, i)
+    print(f"{model_est.params.index[i]:8}: {vif_est:5.4f}")
+
+# Среди эконометристов существует убеждение, что если для одной из переменных >10
+# то в регрессии есть мультиколлинеарность.
+# C(sphere)[T.1]: 2.0648
+# C(sphere)[T.2]: 1.7242
+# C(sphere)[T.3]: 1.2364
+# C(sphere)[T.4]: 1.1382
+# C(sphere)[T.5]: 1.1468
+# C(sphere)[T.6]: 2.3207
+# C(sphere)[T.7]: 3.0952
+# C(sphere)[T.9]: 1.3505
+# C(sphere)[T.10]: 2.2264
+# C(sphere)[T.11]: 1.4432
+# C(sphere)[T.12]: 1.8827
+# C(sphere)[T.13]: 1.7281
+# C(sphere)[T.14]: 3.8372
+# C(sphere)[T.15]: 1.3228
+# C(sphere)[T.16]: 1.2264
+# C(sphere)[T.17]: 2.5153
+# C(sphere)[T.20]: 1.1346
+# C(sphere)[T.21]: 1.2305
+# C(sphere)[T.26]: 1.3353
+# C(sphere)[T.27]: 1.2199
+# C(sphere)[T.28]: 1.2080
+# gender  : 1.4377
+# exp     : 15.5426
+# I(exp ** 2): 12.6149
+# degree  : 1.3316
+# age     : 3.8039
